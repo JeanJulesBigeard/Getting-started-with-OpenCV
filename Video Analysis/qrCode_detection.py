272a26a83ebbfddfc525397b0b1c5d7497e311e1
Jean-Jules Bigeard <48935007+JeanJulesBigeard@users.noreply.github.com>
2020-01-15 13:45:33 +0100

    Add files via upload

---
+++ Video Analysis/qrCode_detection.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import sys
+
+inputImage = cv2.imread("qrcode-learnopencv.jpg")
+
+# Display barcode and QR code location
+def display(im, bbox):
+    n = len(bbox)
+    for j in range(n):
+        cv2.line(im, tuple(bbox[j][0]), tuple(bbox[ (j+1) % n][0]), (255,0,0), 3)
+
+    # Display results
+    cv2.imshow("Results", im)
+
+# Create a qrCodeDetector Object
+qrDecoder = cv2.QRCodeDetector()
+
+# Detect and decode the qrcode
+data,bbox,rectifiedImage = qrDecoder.detectAndDecode(inputImage)
+if len(data)>0:
+    print("Decoded Data : {}".format(data))
+    display(inputImage, bbox)
+    rectifiedImage = np.uint8(rectifiedImage);
+    cv2.imshow("Rectified QRCode", rectifiedImage);
+else:
+    print("QR Code not detected")
+    cv2.imshow("Results", inputImage)
+cv2.imwrite("output.jpg",inputImage)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
